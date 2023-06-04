66e9fea9e9d417c435ce6ce0af41d5ab5b25ec7f
MaxSpiro <maxspiridonov04@gmail.com>
2023-06-04 15:11:34 -0700

    analysis

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,45 +28,104 @@ type Trip = {
   time: Timestamp
 }
 
-// export const tripDuration = onSchedule('*/5 * * * *', async () => {
-//   try {
-//     const sanToIrv = await getTripLength(
-//       coordinates.sanDiego,
-//       coordinates.irvine,
-//     )
-//     const irvToSan = await getTripLength(
-//       coordinates.irvine,
-//       coordinates.sanDiego,
-//     )
+export const tripDuration = onSchedule('*/5 * * * *', async () => {
+  try {
+    const sanToIrv = await getTripLength(
+      coordinates.sanDiego,
+      coordinates.irvine,
+    )
+    const irvToSan = await getTripLength(
+      coordinates.irvine,
+      coordinates.sanDiego,
+    )
 
-//     await Promise.allSettled([
-//       db.collection('trips').add({
-//         destination: 'Irvine',
-//         duration: sanToIrv,
-//         time: Timestamp.now(),
-//       }),
-//       db.collection('trips').add({
-//         destination: 'San Diego',
-//         duration: irvToSan,
-//         time: Timestamp.now(),
-//       }),
-//     ])
-//   } catch (e) {
-//     if (e instanceof AxiosError) {
-//       logger.error(e.response?.data)
-//     }
-//   }
-// })
+    await Promise.allSettled([
+      db.collection('trips').add({
+        destination: 'Irvine',
+        duration: sanToIrv,
+        time: Timestamp.now(),
+      }),
+      db.collection('trips').add({
+        destination: 'San Diego',
+        duration: irvToSan,
+        time: Timestamp.now(),
+      }),
+    ])
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      logger.error(e.response?.data)
+    }
+  }
+})
 
 export const analyzeData = onRequest(async (req, res) => {
   const trips = await db.collection('trips').get()
-  const medianDurations = new Map<number, [number, number]>()
-  logger.log(trips.docs)
-  trips.docs.map((doc, i) => {
+  const sdDurations = new Map<string, number[]>()
+  const irDurations = new Map<string, number[]>()
+  trips.docs.map((doc) => {
     const data = doc.data() as Trip
-    logger.log(data.time.toDate().toLocaleDateString())
-    if (i > 100) return
-    // const medianDuration = medianDurations.get(data.time)
+    const time = data.time.toDate().toISOString().split('T')[1]
+    if (data.destination === 'San Diego') {
+      sdDurations.set(time, [...(sdDurations.get(time) || []), data.duration])
+    } else {
+      irDurations.set(time, [...(irDurations.get(time) || []), data.duration])
+    }
   })
-  res.send('ok')
+  const sdMedians = new Map<string, number>()
+  const irMedians = new Map<string, number>()
+  sdDurations.forEach((durations, time) => {
+    const median = durations.sort((a, b) => a - b)[
+      Math.floor(durations.length / 2)
+    ]
+    sdMedians.set(time, median)
+  })
+  irDurations.forEach((durations, time) => {
+    const median = durations.sort((a, b) => a - b)[
+      Math.floor(durations.length / 2)
+    ]
+    irMedians.set(time, median)
+  })
+
+  const sd = [...sdMedians.entries()]
+    .sort((a, b) => {
+      return a[1] - b[1]
+    })
+    .map(([time, median]) => {
+      const hours = Math.floor(median / 3600)
+      const minutes = Math.floor((median % 3600) / 60)
+      const seconds = median % 60
+
+      const [hour, minute] = time.split(':')
+      const ampm = parseInt(hour) < 12 ? 'AM' : 'PM'
+      const hours12 = parseInt(hour) % 12 || 12
+      return {
+        time: `${hours12}:${minute} ${ampm}`,
+        median: `${hours}h ${minutes}m ${seconds}s`,
+      }
+    })
+  const ir = [...irMedians.entries()]
+    .sort((a, b) => {
+      return a[1] - b[1]
+    })
+    .map(([time, median]) => {
+      const hours = Math.floor(median / 3600)
+      const minutes = Math.floor((median % 3600) / 60)
+      const seconds = median % 60
+
+      const [hour, minute] = time.split(':')
+      const ampm = parseInt(hour) < 12 ? 'AM' : 'PM'
+      const hours12 = parseInt(hour) % 12 || 12
+      return {
+        time: `${hours12}:${minute} ${ampm}`,
+        median: `${hours}h ${minutes}m ${seconds}s`,
+      }
+    })
+
+  await db.collection('analysis').add({
+    sd,
+    ir,
+    time: Timestamp.now(),
+  })
+
+  res.json({ ir, sd })
 })
