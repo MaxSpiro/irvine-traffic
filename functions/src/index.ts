c24a8f6ff0acff0837da389262239c5e1eb839d2
MaxSpiro <maxspiridonov04@gmail.com>
2023-06-04 13:02:50 -0700

    start data analysis

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,6 +13,7 @@ import { coordinates, getTripLength } from './maps'
 import { initializeApp } from 'firebase-admin/app'
 import { getFirestore, Timestamp } from 'firebase-admin/firestore'
 import { AxiosError } from 'axios'
+import { onRequest } from 'firebase-functions/v2/https'
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -21,32 +22,51 @@ initializeApp()
 
 const db = getFirestore()
 
-export const tripDuration = onSchedule('*/5 * * * *', async () => {
-  try {
-    const sanToIrv = await getTripLength(
-      coordinates.sanDiego,
-      coordinates.irvine,
-    )
-    const irvToSan = await getTripLength(
-      coordinates.irvine,
-      coordinates.sanDiego,
-    )
+type Trip = {
+  destination: 'San Diego' | 'Irvine'
+  duration: number
+  time: Timestamp
+}
 
-    await Promise.allSettled([
-      db.collection('trips').add({
-        destination: 'Irvine',
-        duration: sanToIrv,
-        time: Timestamp.now(),
-      }),
-      db.collection('trips').add({
-        destination: 'San Diego',
-        duration: irvToSan,
-        time: Timestamp.now(),
-      }),
-    ])
-  } catch (e) {
-    if (e instanceof AxiosError) {
-      logger.error(e.response?.data)
-    }
-  }
+// export const tripDuration = onSchedule('*/5 * * * *', async () => {
+//   try {
+//     const sanToIrv = await getTripLength(
+//       coordinates.sanDiego,
+//       coordinates.irvine,
+//     )
+//     const irvToSan = await getTripLength(
+//       coordinates.irvine,
+//       coordinates.sanDiego,
+//     )
+
+//     await Promise.allSettled([
+//       db.collection('trips').add({
+//         destination: 'Irvine',
+//         duration: sanToIrv,
+//         time: Timestamp.now(),
+//       }),
+//       db.collection('trips').add({
+//         destination: 'San Diego',
+//         duration: irvToSan,
+//         time: Timestamp.now(),
+//       }),
+//     ])
+//   } catch (e) {
+//     if (e instanceof AxiosError) {
+//       logger.error(e.response?.data)
+//     }
+//   }
+// })
+
+export const analyzeData = onRequest(async (req, res) => {
+  const trips = await db.collection('trips').get()
+  const medianDurations = new Map<number, [number, number]>()
+  logger.log(trips.docs)
+  trips.docs.map((doc, i) => {
+    const data = doc.data() as Trip
+    logger.log(data.time.toDate().toLocaleDateString())
+    if (i > 100) return
+    // const medianDuration = medianDurations.get(data.time)
+  })
+  res.send('ok')
 })
